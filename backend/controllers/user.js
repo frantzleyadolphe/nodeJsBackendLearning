4aaf04d4ebe789c38efcc1cbda03d1af2f1632c6
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-11-16 16:30:10 -0500

    jsonwebtoken logic added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt=require("jsonwebtoken");
+
 exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -18,7 +20,6 @@ exports.signup = (req, res, next) => {
       res.status(500).json({ error });
     });
 };
-
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
@@ -33,7 +34,11 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: "TOKEN",
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                )
               });
             }
           })
