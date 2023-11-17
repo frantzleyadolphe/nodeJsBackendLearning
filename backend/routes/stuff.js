79f74e8601673fa462ec5777877f54b3ea5102cf
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-11-16 21:13:26 -0500

    multer function added

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const stuffCtrl = require("../controllers/stuff");
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 const router = express.Router();
 // data pour l'exemple
 
 router.get("/", auth,stuffCtrl.getAllStuff);
 // create a new record thing
-router.post("/", auth,stuffCtrl.createThing);
+router.post("/", auth,multer,stuffCtrl.createThing);
 router.get("/:id", auth,stuffCtrl.getOneThing);
-router.put("/:id", auth,stuffCtrl.modifyThing);
+router.put("/:id", auth,multer,stuffCtrl.modifyThing);
 router.delete("/:id", auth,stuffCtrl.deleteThing);
 
 module.exports = router;
